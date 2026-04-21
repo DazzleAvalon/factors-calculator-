1cf574c077f0289bd18057592f4fb1f5f7cdfee2
DazzleAvalon <macmenigallb50217@masseyhigh.school.nz>
2026-04-21 13:37:24 +1200

    updated error is to_factor

--- B_01_factors.py
+++ B_01_factors.py
@@ -2,6 +2,7 @@
 def statement_generator(statement, decoration):
     print(f"\n{decoration * 5} {statement} {decoration * 5}")
 
+
 # Displays instructions
 def instructions():
     statement_generator("Instructions", "-")
@@ -16,6 +17,7 @@ Welcome to the factors calculator!
 
 To exit the program, just type "xxx"
    ''')
+
 
 # Ask user for an integer between 1 and 200
 def num_check(question):
@@ -40,9 +42,43 @@ def num_check(question):
             print(error)
 
 
+# Works out factors, returns sorted list
+def get_factors(to_factor):
+        # x**(0.5) is the square root of x
+
+        # we want to loop until we ge the square root of to_factor
+        # stop is the square root of the factor
+        # basically instead of going from one to the number,
+        # we go from 1 to 'stop' (which is the square root
+        # of the number we are trying to factorise)
+
+    factors_list = []
+
+    # square root the number to work out when looping
+    stop = to_factor ** 0.5
+    stop = int(stop)
+
+    for item in range (1, stop + 1):
+
+        # check to see if the item is a factor
+        if to_factor % item == 0:
+            # Add first factor to list
+            factors_list.append(item)
+
+            # find second factor by dividing ' to factor ' by the first factor
+            partner = to_factor // item
+
+            # check second factor is not in list and add it
+            if partner not in factors_list:
+                factors_list.append(partner)
+
+    # output
+    factors_list.sort()
+    return factors_list
+
 # Main routine goes here
 
-statement_generator("The Ultimate Factor Finder", )
+statement_generator("The Ultimate Factor Finder", "-")
 
 # Display instructions if requested
 want_instructions = input("Press <enter> to read the instructions " 
@@ -52,8 +88,44 @@ if want_instructions == "":
     instructions()
 
 while True:
-    to_factor = num_check("To factor: ")
-    print("You chose to factor", to_factor)
+
+    comment = ""
+
+    # ask user for a number to be factorised
+    to_factor = num_check("\nEnter an integer (or xxx to quit)")
 
     if to_factor == "xxx":
         break
+
+    # get factors for integers that are 2 or more
+    elif to_factor != 1:
+        all_factors = factor = (to_factor)
+
+    # set up comment for unity
+    else:
+        all_factors = ""
+        comment = "One is unity! it only has one factor, itself"
+
+    # comments for squares / primes
+
+    # prime numbers have only two factors
+    if len(all_factors) == 2:
+        comment = f"{to_factor} is a prime number"
+
+    # check if the list has an odd number of factors
+    elif len(all_factors) % 2 == 1:
+        comment = f"{to_factor} is a perfect square"
+
+    # Set up headings
+    if to_factor > 1:
+        heading = f"Factors of {to_factor}"
+    else:
+        heading = "One is special..."
+
+    # output factors and comments
+    print()
+    statement_generator(heading, "*")
+    print(all_factors)
+    print(comment)
+
+print("Thank you for using the factors calculator")
